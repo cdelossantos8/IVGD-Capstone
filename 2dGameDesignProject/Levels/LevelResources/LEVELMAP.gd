8c8ecee320bd2a399a0172540b524a6538d19d2c
cdelossantos8 <cdelossantos8@fordham.edu>
2026-04-22 18:41:00 -0400

    Added teleport animation to level map

--- 2dGameDesignProject/Levels/LevelResources/LEVELMAP.gd
+++ 2dGameDesignProject/Levels/LevelResources/LEVELMAP.gd
@@ -24,4 +24,18 @@ func _input(event):
 		$PLAYER.global_position = levels[current_level].global_position
 	
 	if event.is_action_pressed("ui_accept"):
+		$PLAYER/AnimatedSprite2D.stop()
+		
+		var teleport = create_tween()
+		
+		teleport.tween_property($PLAYER/BEAM, "scale", Vector2(1,1), 0.15)
+		teleport.tween_property($PLAYER/AnimatedSprite2D, "scale", Vector2(1,0), 0.1)
+		teleport.tween_property($PLAYER/BEAM, "scale", Vector2(0,1), 0.25).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+		
+		print("TELEPORTED")
+		
+		await teleport.finished
+		
 		get_tree().change_scene_to_file(LOADEDLEVELS[current_level])
+		
+	
