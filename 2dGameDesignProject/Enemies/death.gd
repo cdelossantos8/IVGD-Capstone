4750e05d2d932718f8cc6d02e9affa11da81cc65
cdelossantos8 <cdelossantos8@fordham.edu>
2026-05-07 11:34:51 -0400

    Fixed boss death bug

--- 2dGameDesignProject/Enemies/death.gd
+++ 2dGameDesignProject/Enemies/death.gd
@@ -5,7 +5,7 @@ func enter():
 	owner.set_physics_process(false)
 	animation_player.play('death')
 	await animation_player.animation_finished
-	queue_free()
+	owner.queue_free()
 
 func exit():
 	super.exit()
